d1b35734c7c5d65747901ba46efde0649cc325a0
PrathamDave77 <your-github-email@example.com>
2026-01-30 12:10:49 +0530

    Remove all files

--- src/components/base/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Button = ({variant,children,...props}) => {
-  return (
-    <button className="btn-primary">I am a button</button>
-  )
-}
-
-export default Button
