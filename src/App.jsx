7537e6c7c9e616a227155302b2671539ae54caf5
Pratham <prathamdave37@gmail.com>
2026-01-30 11:57:32 +0530

    Initial Repo

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './index.css'
+import { 
+  Home01, 
+  Settings01, 
+  User01, 
+  Bell01 
+} from "@untitledui/icons";
+import Button from './components/base/Button.jsx';
+
+const App = () => {
+  return (
+    <>
+<button className="btn-primary">Hello</button>
+  <p className = "bg-red-800"> submit</p>
+    <Button/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/base/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({variant,children,...props}) => {
+  return (
+    <button className="btn-primary">I am a button</button>
+  )
+}
+
+export default Button
