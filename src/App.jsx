a2896d406d1f1b5c3f5a7d751814af95bdead548
PrathamDave77 <your-github-email@example.com>
2026-02-02 11:18:52 +0530

    Made heirarchy of css files

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className="bg-[var(--bg-brand-solid)]">asdasds</div>
+    <div className="bg-(--bg-error-solid)">asdasds</div>
   );
 
   
