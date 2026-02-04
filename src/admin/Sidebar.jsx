72b3d14cb48eb552f18f6ed9dfee4d4de0b4efa8
PrathamDave77 <your-github-email@example.com>
2026-02-04 17:39:18 +0530

    Added light dark theme

--- src/admin/Sidebar.jsx
+++ src/admin/Sidebar.jsx
@@ -12,26 +12,31 @@ const Sidebar = () => {
   ]
 
   return (
-    <div className="bg-primary">
-      <div className=''>
-        BlissIq
-      </div>
-      <div>
-        {general.map((item, index) => (
-          <div key={index} className="spacing-lg">
-            {item}
-          </div>
-        ))}
-      </div>
+    <div className="bg-primary radius-none flex space-non flex-col w-[284px] justify-between ">
+      <div id="profile" className="flex flex-row w-[284px] spacing-2xl gap-4xl">
+        <div className='flex flex-col w-[284px] justify-between pr-'>
+          BlissIq
+        </div>
+        <div>
+          {general.map((item, index) => (
+            <div key={index} className="spacing-lg">
+              {item}
+            </div>
+          ))}
+        </div>
 
-      <div className='flex spacing-lg gap-4'>
-        <button className='cursor-pointer'>
-          light
-        </button>
-        <button className='cursor-pointer' onClick={() => document.documentElement.classList.toggle('theme-dark')}>
-          dark
-        </button>
+        <div className='flex spacing-lg gap-4'>
+          <button className='cursor-pointer' onClick={() => document.documentElement.classList.remove("theme-dark")}>
+            light
+          </button>
+          <button className='cursor-pointer' onClick={() => document.documentElement.classList.add('theme-dark')}>
+            dark
+          </button>
+        </div>
       </div>
+      <div id="settings">
+
+      </div>  
       
     </div>
   )
